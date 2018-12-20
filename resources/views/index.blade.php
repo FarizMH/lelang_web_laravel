f488db2d55a2f782c402dc7fd93281234983e405
FarizMH <fariz.herman@gmail.com>
2018-12-20 07:50:52 +0700

    ini pk larave tp baru register aja loginya blm

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  @include('items.head')
+
+  <body>
+
+    
+    @include('items.nav')
+    @include('items.header')
+    @include('items.content')
+    @include('items.footer')
+    <!-- Page Content -->
+    
+
+     
+
+    <!-- Footer -->
+   
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
